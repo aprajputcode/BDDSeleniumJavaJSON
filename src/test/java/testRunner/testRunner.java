a382acd80e41b0fb8331699cfe29ffbb0c85627c
akshay <akshay.pardeshi.7373@gmail.com>
2022-07-22 19:10:32 +0530

    Adding extent report

--- src/test/java/testRunner/testRunner.java
+++ src/test/java/testRunner/testRunner.java
@@ -7,12 +7,14 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "./src/test/java/FeaturePackage/"
+		features = "./src/test/java/FeaturePackage/first.feature"
 		,glue={"stepDefinaction"}
 		,dryRun=false
-		,tags= "@sanity"
-		,plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
-//				"html:target/cucumber-html-report/report.html","json:target/cucumber-report/cucumber.json","junit:target/cucumber-report/cucumber.xml"}
+		//,tags= "@sanity"
+		,plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"html:target/cucumber-html-report/report.html",
+				"json:target/cucumber-report/cucumber.json",
+				"junit:target/cucumber-report/cucumber.xml"}
 		,monochrome=true
 		)
 public class testRunner {
